1a9a083fc81241554aa02cb0413f062143cce423
wanjiling201819801 <3293664706@qq.com>
2020-11-24 20:52:39 +0800

    001

---
+++ src/OralArithmeticTraining_v02.java
@@ -0,0 +1,58 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class OralArithmeticTraining_v02 {
+    public static void main(String[] args) {
+        //0 符号，1、2 前后两个操作数,3 正确答案结果,4 用户答案
+        short[][] arr = new short[10][5];
+        FormulaGeneration(arr);     //随机生成算式
+        ProblemSetGeneration(arr);  //生成习题集
+        AddSubCalUserInput(arr);    //加减法计算，存储用户答案
+        PrintResults(arr);          //打印结果
+
+    }
+    //随机生成算式
+    public static void FormulaGeneration(short arr[][]){
+        Random random = new Random();
+        for (int i = 0; i < arr.length; i++) {
+            ///生成‘+’、‘-’符号
+            arr[i][0] = (short)random.nextInt(2);
+            //产生100以内的随机数
+            arr[i][1] = (short)random.nextInt(101);
+            arr[i][2] = (short)random.nextInt(101);
+        }
+    }
+    //生成习题集
+    public static void ProblemSetGeneration(short arr[][]){
+        char o = '+';
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i][0]==0){
+                o = '+';
+                //存储正确答案
+                arr[i][3] = (short)(arr[i][1] + arr[i][2]);
+            }else{
+                o = '-';
+                arr[i][3] = (short)(arr[i][1] - arr[i][2]);
+            }
+            //显示习题
+            System.out.println("No."+(i+1)+":\t"+arr[i][1]+o+arr[i][2]+"=");
+        }
+    }
+    //加减法计算，存储用户答案
+    public static void AddSubCalUserInput(short arr[][]){
+        Scanner sin = new Scanner(System.in);
+        for (int i = 0; i <arr.length; i++) {
+            arr[i][4] = (short)sin.nextInt();
+        }
+    }
+    //打印结果
+    public static void PrintResults(short arr[][]){
+        for (int i = 0; i < arr.length ; i++) {
+            if(arr[i][4] == arr[i][3]  ){
+                System.out.println("结果正确");
+            }else{
+                System.out.println("结果错误");
+            }
+        }
+    }
+}
---
+++ src/OralArithmeticTraining_v03.java
@@ -0,0 +1,109 @@
+import java.util.Random;
+import java.util.Scanner;
+public class OralArithmeticTraining_v03 {
+    public static int Max = 100;
+    public static void main(String[] args) {
+        //0 符号，1、2 前后两个操作数,3 正确答案结果,4 用户答案
+        short[][] arr = new short[10][5];
+        FormulaGeneration(arr);     //随机生成算式
+        PrintProblemSet(arr);       //打印习题集
+        AddSubCalUserInput(arr);    //加减法计算，存储用户答案
+        PrintResults(arr);          //打印结果
+    }
+    //随机生成操作数（Max以内的数和负数）
+    //[-Max,Max] = random(2*Max+1)-Max
+    //正负数以及最大值都是可调的
+    public static short OperandsRange(){
+        Random random = new Random();
+        return (short)(random.nextInt(2*Max+1)-Max);
+    }
+    //满足隐含条件的两个操作数
+    public static void GetOperands(short ov,short num[]){
+        num[0] = OperandsRange();
+        num[1]= OperandsRange();
+        //加法 和不超过100
+        if(ov==0){
+            while(num[0]+num[1]>100){
+                num[0] = OperandsRange();
+                num[1] = OperandsRange();
+            }
+        }else{
+            //减法 差不能小于0
+            while(num[0]-num[1]<0){
+                num[0] = OperandsRange();
+                num[1] = OperandsRange();
+            }
+        }
+    }
+    //生成符号
+    public static short Operator(){
+        Random random = new Random();
+        short ratio = (short)random.nextInt(100);
+        //控制生成符号的比例大约是 加：减=3：1
+        if(ratio<75){
+            return 0;
+        }else{
+            return 1;
+        }
+    }
+    //生成算式
+    public static void FormulaGeneration(short arr[][]){
+        short num[] = new short[2];
+        for (int i = 0; i < arr.length; i++){ // 0 1 2 3
+            //符号
+            arr[i][0] = Operator();  // 1 0 0 0
+            //得到两个数
+            GetOperands(arr[i][0],num); //1 2 3 4  3 4 5 2
+            arr[i][1]=num[0];  // 1 2 3 4
+            arr[i][2]=num[1];  // 3 4 5 2
+        }
+    }
+    //生成习题集
+    public static void PrintProblemSet(short arr[][]){
+        char o = '+';
+        String str = null;
+        for (int i = 0; i < arr.length; i++) {
+            //存储正确答案
+            if(arr[i][0]==0){
+                o = '+';
+                arr[i][3] = (short)(arr[i][1] + arr[i][2]);
+            }else{
+                o = '-';
+                arr[i][3] = (short)(arr[i][1] - arr[i][2]);
+            }
+            //显示习题
+            int j= i%3;
+            if(arr[i][2]<0){
+                str="No."+(i+1)+": "+arr[i][1]+o+"("+arr[i][2]+")=";
+            }else{
+                str="No."+(i+1)+": "+arr[i][1]+o+arr[i][2]+"=";
+            }
+            while(str.length()<25){
+                str+=' ';
+            }
+            System.out.print(str);
+            if(j==2){
+                System.out.println();
+            }
+        }
+        System.out.println();
+    }
+    //加减法计算，输入并存储用户答案
+    public static void AddSubCalUserInput(short arr[][]){
+        Scanner sin = new Scanner(System.in);
+        for (int i = 0; i <arr.length; i++) {
+            arr[i][4] = (short)sin.nextInt();
+        }
+    }
+    //打印结果
+    public static void PrintResults(short arr[][]){
+        for (int i = 0; i < arr.length ; i++) {
+            if(arr[i][4] == arr[i][3]  ){
+                System.out.println("结果正确");
+            }else{
+                System.out.println("结果错误");
+            }
+        }
+    }
+}
+
